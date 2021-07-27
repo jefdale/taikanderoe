006fe77344c785f7480fbfcc2831d7b461af278c
ITBCA <jefebry.dale@gmail.com>
2021-07-27 13:37:52 +0700

    adada

---
+++ andore/src/main/java/com/example/andore/CalculatorandoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.andore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorandoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorandoreApplication.class, args);
+	}
+
+}
---
+++ andore/src/test/java/com/example/andore/CalculatorandoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.andore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorandoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
